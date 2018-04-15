fd0ea948ee2c8a38ceda878d1febf7f33f86f706
Sofia Denner <sofi.denner@gmail.com>
2018-04-15 19:55:04 -0300

    name home url

--- scatchbling_api/backscratcher/urls.py
+++ scatchbling_api/backscratcher/urls.py
@@ -5,6 +5,6 @@ from backscratcher import views
 
 urlpatterns = [
     path('api/', include('backscratcher.urls_api')),
-    path('', views.home),
+    path('', views.home, name='home'),
     path('bs/<int:bs_pk>/', views.backscratcher_detail),
 ]
