45face3f2935af53af88b5b6211bf564870976c0
Sofia Denner <sofi.denner@gmail.com>
2018-04-16 02:01:55 -0300

    basic and token auth enabled

--- scatchbling_api/scatchbling_api/settings.py
+++ scatchbling_api/scatchbling_api/settings.py
@@ -123,6 +123,7 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     )
 }
 
