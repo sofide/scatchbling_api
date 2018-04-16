1a11328e0753dc704421a716f454435abbe5f488
Sofia Denner <sofi.denner@gmail.com>
2018-04-16 01:43:23 -0300

    url to get api token

--- scatchbling_api/backscratcher/urls_api.py
+++ scatchbling_api/backscratcher/urls_api.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.authtoken import views as token_views
+
 from backscratcher import views_api as views
 
 
 urlpatterns = [
     url(r'^$', views.BackscratcherList.as_view()),
     url(r'^(?P<pk>[0-9]+)/?$', views.BackscratcherDetail.as_view()),
+    url(r'^api-token-auth/', token_views.obtain_auth_token),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
